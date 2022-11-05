047ff8156a490293f6f583ca5656c9aed8c61104
choisungwook <kgg1959@naver.com>
2022-11-05 09:59:00 +0900

    week3-layout backend수정

--- week3/layout/dev/backend.tf
+++ week3/layout/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "$NICKNAME-t101study-tfstate-week3-files"
+    bucket = "akbun-t101study-tfstate"
     key    = "dev/terraform.tfstate"
     region = "ap-northeast-2"
     dynamodb_table = "terraform-locks-week3-files"
