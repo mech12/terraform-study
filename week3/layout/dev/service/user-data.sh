4e605a8c449ccad5ef605461f7e37c8f977ee37f
choisungwook <kgg1959@naver.com>
2022-11-05 13:59:05 +0900

    week3-layout ALB user-data intsnace ID추가

--- week3/layout/dev/service/user-data.sh
+++ week3/layout/dev/service/user-data.sh
@@ -2,9 +2,12 @@
 wget https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64
 mv busybox-x86_64 busybox
 chmod +x busybox
+IID=$(curl 169.254.169.254/latest/meta-data/instance-id)
 
 cat > index.html <<EOF
 <h1>T101 Study</h1>
+<p>Hello</p>
+<p>Instance ID($IID)!!</p>
 <p>My RDS DB address: ${db_address}</p>
 <p>My RDS DB port: ${db_port}</p>
 EOF
