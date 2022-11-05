a79e68a91ef956d2c82d6cf5853839eb74d3c4c1
choisungwook <kgg1959@naver.com>
2022-11-05 09:49:33 +0900

    3주차 레이아웃 오타수정

--- week3/layout/global/s3.tf
+++ week3/layout/global/s3.tf
@@ -1,12 +1,3 @@
-provider "aws" {
-  region = "ap-northeast-2"
-}
-
 resource "aws_s3_bucket" "mys3bucket" {
   bucket = "akbun-t101study-tfstate"
-}
-
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.mys3bucket.arn
-  description = "The ARN of the S3 bucket"
 }
