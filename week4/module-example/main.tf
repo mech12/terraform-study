05c0f49df3eeaad487d0a4b0c6c1a1585db10f4d
choisungwook <kgg1959@naver.com>
2022-11-09 11:24:29 +0900

    week4 모듈추가

---
+++ week4/module-example/main.tf
@@ -0,0 +1,5 @@
+module "my-s3" {
+  source = "./my-s3"
+
+  bucket_name = "akbun-t101-week4-local-example"
+}
---
+++ week4/module-example/my-s3/variable.tf
@@ -0,0 +1,6 @@
+variable "bucket_name" {
+  description = "bucket name"
+  type        = string
+  sensitive   = true
+  default = "akbun-t101-week4-local-example"
+}
