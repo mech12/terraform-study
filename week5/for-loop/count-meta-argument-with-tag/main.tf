e7e64332bdcf790b50750fa5eedbc6800a5e9663
choisungwook <kgg1959@naver.com>
2022-11-15 13:14:36 +0900

    week5 count with index

---
+++ week5/for-loop/count-meta-argument-with-tag/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+data "aws_ami" "last_ami" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "server" {
+  count = 2
+
+  ami           = data.aws_ami.last_ami.id
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "Server ${count.index}"
+  }
+}
