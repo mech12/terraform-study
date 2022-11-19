42e6a37d4c12f2687586a76462a9de24e6a3ad69
choisungwook <kgg1959@naver.com>
2022-11-19 13:20:55 +0900

    week5 for expression

---
+++ week5/for-loop/for-expression-variable/main.tf
@@ -0,0 +1,21 @@
+variable "fruits" {
+  type        = set(string)
+  default     = ["apple", "banana"]
+  description = "fruit example"
+}
+
+variable "postfix" {
+  type        = string
+  default     = "test"
+  description = "postfix"
+}
+
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_s3_bucket" "mys3bucket" {
+  for_each = toset([for fruit in var.fruits : format("%s-%s", fruit, var.postfix)])
+  bucket   = "akbun-t101study-${each.key}"
+}
