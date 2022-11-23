0b9af9e82ec7a0428ff1c63201c00e02e843ff77
choisungwook <kgg1959@naver.com>
2022-11-23 21:59:32 +0900

    week6 ec2 instnace with ssh

---
+++ week6/ec2_instance_with_ssh/Readme.md
@@ -0,0 +1,19 @@
+# 개요
+* aws ami EC2 Instance 실행
+
+# 준비
+* ssh 키쌍 생성
+```sh
+ssh-keygen -t rsa -b 4096 -f demo
+```
+
+# 실행
+* 약 1분 소요
+```sh
+terraform init
+terraform apply
+```
+
+# 참고자료
+* https://www.letscloud.io/community/how-to-install-jenkins-on-ubuntu-2004
+* https://medium.com/@raguyazhin/install-jenkins-on-amazon-linux-ec2-instance-using-terraform-1a6bc35bacaa
---
+++ week6/ec2_instance_with_ssh/ec2.tf
@@ -0,0 +1,63 @@
+resource "aws_key_pair" "demo" {
+  key_name   = "demo"
+  public_key = file("./demo.pub")
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.demo.key_name
+  vpc_security_group_ids = [aws_security_group.stg_mysg.id]
+
+  iam_instance_profile = aws_iam_instance_profile.instance.name
+}
+
+resource "aws_iam_role" "instance" {
+  name_prefix        = var.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "example" {
+  role   = aws_iam_role.instance.id
+  policy = data.aws_iam_policy_document.ec2_admin_permissions.json
+}
+
+data "aws_iam_policy_document" "ec2_admin_permissions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_instance_profile" "instance" {
+  role = aws_iam_role.instance.name
+}
