1c7176a8b09936e3c5c41a51061eb2a197b1d98e
choisungwook <kgg1959@naver.com>
2022-11-25 22:29:04 +0900

    week6 aws-oidc role 추가

--- week6/github_action_aws-oidc/main.tf
+++ week6/github_action_aws-oidc/main.tf
@@ -1,11 +1,17 @@
-resource "aws_iam_openid_connect_provider" "github-oidc" {
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+
+# reference: https://stackoverflow.com/questions/69243571/how-can-i-connect-github-actions-with-aws-deployments-without-using-a-secret-key/69243572#69243572
+resource "aws_iam_openid_connect_provider" "github_oidc" {
   url = "https://token.actions.githubusercontent.com"
 
   client_id_list = [
     "sts.amazonaws.com"
   ]
 
-  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 data "aws_iam_policy_document" "github_allow" {
@@ -14,7 +20,7 @@ data "aws_iam_policy_document" "github_allow" {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github-oidc.arn]
+      identifiers = [aws_iam_openid_connect_provider.github_oidc.arn]
     }
     condition {
       test     = "StringLike"
@@ -25,7 +31,33 @@ data "aws_iam_policy_document" "github_allow" {
   }
 }
 
+data "aws_iam_policy_document" "policy_bucket_list" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+}
+
 resource "aws_iam_role" "github_role" {
   name               = "GithubActionsRole"
   assume_role_policy = data.aws_iam_policy_document.github_allow.json
+}
+
+resource "aws_iam_role_policy" "example" {
+  role   = aws_iam_role.github_role.id
+  policy = data.aws_iam_policy_document.policy_bucket_list.json
 }
